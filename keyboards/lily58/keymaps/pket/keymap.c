e392246ab74179a080dd2ea65ae7850e473ed254
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2021-04-06 10:25:57 +0200

    Before rebase

--- keyboards/lily58/keymaps/pket/keymap.c
+++ keyboards/lily58/keymaps/pket/keymap.c
@@ -1,10 +1,15 @@
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
+#define LT_LBRC TD(TD_LT_LBRC) 
+#define GT_RBRC TD(TD_GT_RBRC) 
+
 #define RAISE_F LT(_RAISE, KC_F)
 #define RAISE_J LT(_RAISE, KC_J)
 #define SYM_D LT(_SYMBOL, KC_D)
-#define SFT_ENT RSFT_T(KC_ENT)
+#define RAISE_ENT LT(_RAISE, KC_ENT)
+#define NUM_TAB LT(_NUMBER, KC_TAB)
+#define RAISE_ESC LT(_RAISE, KC_ESC)
 
 #define RAISE MO(_RAISE)
 #define NUMBER MO(_NUMBER)
@@ -23,9 +28,17 @@
 #define SWE_AE RALT(KC_Q)
 #define SWE_OE RALT(KC_P)
 
-#define SCLN_OE TD(TD_OE)
-#define QUOT_AE TD(TD_AE)
-#define MINS_AO TD(TD_AO)
+// Left-hand home row mods
+#define HOME_A LCTL_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LGUI_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RGUI_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCN RCTL_T(KC_SCLN)
 
 enum layer_number {
   _QWERTY = 0,
@@ -33,6 +46,7 @@ enum layer_number {
   _RAISE,
   _ADJUST,
   _NUMBER,
+  _NUMPAD,
   _SYMBOL,
 };
 
@@ -42,117 +56,52 @@ enum custom_keycodes {
 };
 
 enum combos {
-    CB_KL_CLN,
+//    CB_KL_CLN,
     CB_DIR_UP,
 };
 
-const uint16_t PROGMEM kl_combo[] = {KC_K, KC_L, COMBO_END};
+//const uint16_t PROGMEM kl_combo[] = {KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM dir_up_combo[] = {KC_COMM, KC_DOT, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-    [CB_KL_CLN] = COMBO(kl_combo, KC_COLN),
+ //   [CB_KL_CLN] = COMBO(kl_combo, KC_COLN),
     [CB_DIR_UP] = COMBO_ACTION(dir_up_combo),
 };
 
-// TAP DANCE
-typedef struct {
-    uint16_t keycode;
-} test_user_data_t;
-
-#define ACTION_TAP_DANCE_FN_ADVANCED_USER(user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset, user_user_data) \
-        { .fn = {user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset}, .user_data = (void*)user_user_data, }
-
-void td_swe_each_tap(qk_tap_dance_state_t *state, void *user_data) {
-    uint16_t keycode = ((test_user_data_t*)user_data)->keycode;
-    switch (state->count) {
-        case 1 ... 2:
-            break;
-        case 3:
-            tap_code16(keycode);
-            tap_code16(keycode);
-            break;
-        default:
-            tap_code16(keycode);
-            break;
-    }
-}
-
-void td_swe_finished(qk_tap_dance_state_t *state, void *user_data) {
-    uint16_t keycode = ((test_user_data_t*)user_data)->keycode;
-    if (state->count == 1) {
-        register_code16(keycode);
-    } else if (state->count == 2) {
-        register_mods(MOD_BIT(KC_RALT));
-        switch(keycode) {
-            case KC_SCLN:
-                register_code16(KC_P);
-                break;
-            case KC_QUOT:
-                register_code16(KC_Q);
-                break;
-            case KC_MINS:
-                register_code16(KC_W);
-                break;
-        }
-    }
-}
-
-void td_swe_reset(qk_tap_dance_state_t *state, void *user_data) {
-    uint16_t keycode = ((test_user_data_t*)user_data)->keycode;
-    if (state->count == 1) {
-        unregister_code16(keycode);
-    } else if(state->count == 2) {
-        unregister_mods(MOD_BIT(KC_RALT));
-        switch(keycode) {
-            case KC_SCLN:
-                unregister_code16(KC_P);
-                break;
-            case KC_QUOT:
-                unregister_code16(KC_Q);
-                break;
-            case KC_MINS:
-                unregister_code16(KC_W);
-                break;
-        }
-    } 
-}
 enum {
-    TD_OE,
-    TD_AE,
-    TD_AO,
+    TD_LT_LBRC,
+    TD_GT_RBRC,
 };
 
+// Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_OE] = ACTION_TAP_DANCE_FN_ADVANCED_USER(td_swe_each_tap, td_swe_finished, td_swe_reset, &((test_user_data_t){KC_SCLN})),
-    [TD_AE] = ACTION_TAP_DANCE_FN_ADVANCED_USER(td_swe_each_tap, td_swe_finished, td_swe_reset, &((test_user_data_t){KC_QUOT})),
-    [TD_AO] = ACTION_TAP_DANCE_FN_ADVANCED_USER(td_swe_each_tap, td_swe_finished, td_swe_reset, &((test_user_data_t){KC_MINS})),
+    // Tap once for Escape, twice for Caps Lock
+    [TD_LT_LBRC] = ACTION_TAP_DANCE_DOUBLE(KC_LT, KC_LBRC),
+    [TD_GT_RBRC] = ACTION_TAP_DANCE_DOUBLE(KC_GT, KC_RBRC),
 };
 
 // TODO
-// - Move SWE handling to tap dance?
 // - Should I perhaps disable the num row all together
-// - Move Ctrl and Alt to Mod tap
 // - Use Alt button and RGUI for something better, like layer toggle
 //   for some rarely used layer like media or function keys.
 // - Figure out if the left and right arrows belong on small layers
 // - Add more small layers centered around the F and J
-// - Add suspend block with oled off 
 // - Create a pure media/system layer
 // - Add a layer for toggling state, like Caps Lock and Win/Mac mode
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    MINS_AO,
-  CTL_ESC, KC_A,    KC_S,    SYM_D,   RAISE_F, KC_G,                        KC_H,    RAISE_J, KC_K,    KC_L,    SCLN_OE, QUOT_AE,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+  CTL_ESC, HOME_A,  HOME_S,  HOME_D,  HOME_F,  KC_G,                        KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCN,KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                             KC_LALT, KC_LGUI, NUMBER,  LOW_SPC,  SFT_ENT,  RAISE,   KC_BSPC, KC_RALT
+                             KC_LALT, KC_LGUI, NUM_TAB, LOW_SPC,  RAISE_ENT,RAISE_ESC,   KC_BSPC, KC_RALT
 ),
 
 [_LOWER] = LAYOUT(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   _______, _______, _______, PRV_TAB, NXT_TAB, _______,                     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, SWE_AO,
-  _______, _______, _______, _______, _______, _______,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, SWE_OE,  SWE_AE,
+  _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, SWE_OE,  SWE_AE,
   _______, _______, _______, _______, _______, _______,  _______, _______,  CTL_LFT, CTL_DWN, CTL_UP,  CTL_RGT, _______, _______,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
 ),
@@ -165,14 +114,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              _______, _______, _______,  _______, _______,  _______, _______, _______
 ),
 
-[_SYMBOL] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, KC_PLUS, _______,                     _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
-  _______, _______, _______, _______, KC_EQL,  _______,                     _______, KC_LPRN, KC_RPRN, _______, _______, _______,
-  _______, _______, _______, _______, KC_MINS, KC_LEFT,  _______, _______,  KC_RGHT, KC_PIPE, KC_AMPR, _______, _______, _______,
-                             _______, _______, _______,  _______, _______,  _______, _______, _______
-),
-
 [_NUMBER] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
@@ -180,6 +121,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, KC_LEFT,  _______, _______,  KC_RGHT, _______, _______, _______, _______, _______,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
 ),
+
+// BEGIN New stuff for home row mods
+[_NUM_LEFT] = LAYOUT(
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+  _______, _______, KC_7,    KC_8,    KC_9,    KC_0,                        KC_PLUS, _______, _______, _______, _______, _______,
+  _______, _______, KC_4,    KC_5,    KC_6,    KC_SLSH,                     KC_EQL,  KC_RSFT, KC_RGUI, KC_LALT, KC_RCTL, _______,
+  _______, _______, KC_1,    KC_2,    KC_3,    KC_ASTR,  _______, _______,  KC_MINS, _______, _______, _______, _______, _______,
+                             _______, _______, _______,  _______, _______,  _______, _______, _______
+),
+
+[_S_NUM_LEFT] = LAYOUT(
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+  _______, _______, KC_AMPR, KC_ASTR, KC_LPRN, KC_EQL,                      KC_PLUS, _______, _______, _______, _______, _______,
+  _______, _______, KC_DLR,  KC_PERC, KC_CIRC, _______,                     KC_EQL,  KC_RSFT, KC_RGUI, KC_LALT, KC_RCTL, _______,
+  _______, _______, KC_EXLM, KC_AT,   KC_HASH, _______,  _______, _______,  KC_MINS, _______, _______, _______, _______, _______,
+                             _______, _______, _______,  _______, _______,  _______, _______, _______
+),
+
+[_SYM_RIGHT] = LAYOUT( // TODO create a system layer to put the ../ and ~/ keys on, combine with modified arrow keys
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                     KC_PLUS, KC_PIPE, KC_LCBR, KC_RCBR, KC_AMPR, KC_UNDS,
+  _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______,                     KC_EQL,  KC_EXLM, KC_LPRN, KC_RPRN, KC_COLN, KC_DQUO,
+  _______, _______, _______, _______, _______, _______,  _______, _______,  KC_MINS, KC_DLR,  LT_LBRC, GT_RBRC, KC_QUES, KC_BSLS,
+                             _______, _______, _______,  _______, _______,  CKC_ARW, KC_LEFT, KC_RGHT
+),
+// END 
 
 [_ADJUST] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -201,11 +168,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch(keycode) {
-        case TD(TD_OE):
-            return TAPPING_TERM;
-        case TD(TD_AE):
-        case TD(TD_AO):
-            return TAPPING_TERM + 25;
+        case LGUI_T(KC_D):
+        case RGUI_T(KC_K):
+            return TAPPING_TERM + 75;
+        case LALT_T(KC_S):
+        case LALT_T(KC_L):
+            return TAPPING_TERM + 75;
+        case LCTL_T(KC_A):
+        case RCTL_T(KC_SCLN):
+            return TAPPING_TERM + 75;
         case LT(_LOWER, KC_SPC):
             return TAPPING_TERM + 75;
         default:
@@ -325,7 +296,7 @@ void dynamic_macro_record_end_user(int8_t direction) {
 }
 
 void render_keylogger_status(void) {
-    oled_write_P(PSTR("Hist."), false);
+    oled_write_P(PSTR("Keys "), false);
     oled_write(keylog_str, false);
 }
 
@@ -341,7 +312,6 @@ void render_dynamic_macro_state(void) {
     oled_write_P(dm_1_set ? PSTR(".") : PSTR(" "), false);
     oled_write_P(PSTR("R"), false);
     oled_write_P(dm_2_set ? PSTR(".") : PSTR(" "), false);
-
 
     if (recording_macro) {
         render_dm_record();
@@ -390,10 +360,10 @@ void render_keylock_status(led_t led_state) {
 
 void render_mod_status(uint8_t modifiers) {
     oled_write_ln_P(PSTR("Mods"), false);
-    oled_write_P(PSTR("S"), (modifiers & MOD_MASK_SHIFT));
     oled_write_P(PSTR("C"), (modifiers & MOD_MASK_CTRL));
     oled_write_P(PSTR("A"), (modifiers & MOD_MASK_ALT));
     oled_write_P(PSTR("G"), (modifiers & MOD_MASK_GUI));
+    oled_write_P(PSTR("S"), (modifiers & MOD_MASK_SHIFT));
     oled_write_P(PSTR(" "), false);
 }
 
@@ -475,6 +445,8 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
     switch(combo_index) {
         case CB_DIR_UP:
             if (pressed) {
+// - Add suspend block with oled off 
+// - Add suspend block with oled off 
                 SEND_STRING("../");
             }
             break;
