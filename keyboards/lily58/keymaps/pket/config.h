411389d3fc394deb619f2c92366d76201c91d1a5
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2021-07-17 12:37:19 +0200

    Far from viable for work

--- keyboards/lily58/keymaps/pket/config.h
+++ keyboards/lily58/keymaps/pket/config.h
@@ -20,11 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* Select hand configuration */
-
 #define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
 
 #define TAPPING_TERM 200
 //#define TAPPING_TERM_PER_KEY
